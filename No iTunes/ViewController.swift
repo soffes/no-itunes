58cc7b20765cf57054fe57952a39a1b58bf34422
Sam Soffes <sam@soff.es>
2016-12-04 18:36:07 -0800

    Initial Commit

---
+++ No iTunes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  No iTunes
+//
+//  Created by Sam Soffes on 12/4/16.
+//  Copyright © 2016 Sam Soffes. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
